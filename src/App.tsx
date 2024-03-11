29d0bd1f729521c5b8677822b1591c3f8cddc02f
hugogouveiasardinha <hgsardinha@gmail.com>
2024-03-11 16:39:25 +0100

    get request for character name to guess

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,55 @@
-function App() {
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-  return (
-    <h1>Hi</h1>
-
-  )
+interface CharacterData {
+  id: number;
+  name: string;
+  gender: string;
+  marital: string;
+  job: string[];
 }
 
-export default App
+function App() {
+  const [nameToGuess, setNameToGuess] = useState('');
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  useEffect(() => {
+    // Vérification si le composant est monté
+    let isMounted = true;
+
+    const generateRandomNumber = () => Math.floor(Math.random() * 83) + 1;
+    const randomNumber = generateRandomNumber();
+    console.log(randomNumber);
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<CharacterData>(`https://theofficeapi.dev/api/character/${randomNumber}`, {
+          params: {
+            includeEpisodes: false,
+          },
+        });
+
+        if (isMounted) {
+          // Si le composant est toujours monté, effectuer les mises à jour nécessaires
+          const characterName = response.data.name;
+          console.log(response.data);
+          setNameToGuess(characterName);
+        }
+      } catch (error) {
+        console.error('Erreur lors de la requête API:', error.message);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  return (
+    <h1>hi {nameToGuess}</h1>
+  );
+}
+
+export default App;

37efa6b2e23ce2cf22e7a4efb463211bde03da51
hugogouveiasardinha <hgsardinha@gmail.com>
2024-03-13 16:46:53 +0100

    ha,gman body legs and arms

--- src/components/HangmanDrawing/HangmanDrawing.tsx
+++ src/components/HangmanDrawing/HangmanDrawing.tsx
@@ -1,13 +1,43 @@
 import './HangmanDrawing.scss'
 
-export function HangmanDrawing(){
-return (
-    <div className="Drawing">
-        <div className="Hang-stick" />
-        <div className="Top-stick" />
-        <div className="Pole-stick" />
-        <div className="Base-stick" />
-    </div>
-)
+const head = (
+    <div className="Head" />
+);
+
+const body = (
+    <div className="Body" />
+);
+
+const rightArm = (
+    <div className="Right-arm" />
+);
+
+const leftArm = (
+    <div className="Left-arm" />
+);
+
+const rightLeg = (
+    <div className="Right-leg" />
+);
+
+const leftLeg = (
+    <div className="Left-leg" />
+);
+
+export function HangmanDrawing() {
+    return (
+        <div className="Drawing">
+            {head}
+            {body}
+            {rightArm}
+            {leftArm}
+            {rightLeg}
+            {leftLeg}
+            <div className="Hang-stick" />
+            <div className="Top-stick" />
+            <div className="Pole-stick" />
+            <div className="Base-stick" />
+        </div>
+    )
 
 }
